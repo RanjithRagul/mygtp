d66d9fe55fa750bcea435c1da8c344d5ffa72907
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-08-24 22:48:25 +0530

    Revise docstring in forward method
    Updated docstring for forward method to clarify input normalization process and parameters.

--- train.py
+++ train.py
@@ -9,22 +9,22 @@ class LayerNorm(nn.Module):
     self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
     
   def forward(self, input: Tensor) -> Tensor:
-   """
-   input -> tensor, weight shape, weight, bias, epsilon to tackel zero division error
-   1. normalize the input tensor: 
-   mean = sum(input)/len(input)
-   #--------------------------------
-   variance = 0
-   for n in input:
-     variance += (n - mean)**2
-   variance /= len(input)
-   #--------------------------------
-   normalized = (x - mean) / sqrt(variance + tiny number to tackle divisible by zero error)
-   NOTE: mean, variance = integer, tiny = small decimal value, x = Tensor
-   #--------------------------------
-   2. input @ weight + bias
-   return the input
-   1e-5 = 0.00001
-   """
     return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+    """
+    input -> tensor, weight shape, weight, bias, epsilon to tackel zero division error
+    1. normalize the input tensor: 
+    mean = sum(input)/len(input)
+    #--------------------------------
+    variance = 0
+    for n in input:
+      variance += (n - mean)**2
+    variance /= len(input)
+    #--------------------------------
+    normalized = (x - mean) / sqrt(variance + tiny number to tackle divisible by zero error)
+    NOTE: mean, variance = integer, tiny = small decimal value, x = Tensor
+    #--------------------------------
+    2. input @ weight + bias
+    return the input
+    1e-5 = 0.00001
+    """
     

9506e46d1da94579964fd773c0d97b23f49a889a
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-08-25 14:40:29 +0530

    Implement dropout layers in train.py
    Added dropout layers for attention and residual connections.

--- train.py
+++ train.py
@@ -42,3 +42,7 @@ class CausalSelfAttention(nn.Module):
     4. weight = torch.randn(out_features, in_features)
     5. bias = torch.randn(out_features)
     '''
+    # remember dropout is always between 0 - 1
+    # nn.Dropout(value, inplace=True/False) -> this in place is to create a new tensor or inplace
+    self.attn_dropout = nn.Dropout(config.dropout)
+    self.resid_dropout = nn.Dropout(config.dropout)

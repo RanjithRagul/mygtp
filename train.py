6988f50e5c8ddcfe88d5760c5bdec0a704a8479f
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-08-28 23:21:58 +0530

    Fix wpe embedding size to match block size

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ class GTP(nn.module):
 
 		self.transformer = nn.ModuleDict(dict(
 			wte = nn.Embedding(config.vocab_size, config.n_embd),
-			wpe = nn.Embedding(config.vocab_size, config.n_embd),
+			wpe = nn.Embedding(config.block_size, config.n_embd),
 			drop = nn.Dropout(config.dropout),
 			h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
 			ln_f = LayerNorm(config.n_embd, bias=config.bias)

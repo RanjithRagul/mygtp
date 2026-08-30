5411374b73b0857b3c09254429eecda8b047cafe
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-08-30 15:14:43 +0530

    Fix bias initialization in _init_weights method

--- model.py
+++ model.py
@@ -173,7 +173,7 @@ class GTP(nn.Module):
 		if isinstance(module, nn.Linear):
 			nn.init.normal_(module.weight, mean=0.0, std=0.02)
 			if module.bias is not None:
-				nn.init.zeros(module.bias)
+				nn.init.zeros_(module.bias)
 		elif isinstance(module, nn.Embedding):
 			nn.init.normal_(module.weight, mean=0.0, std=0.02)
 

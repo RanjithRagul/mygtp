bcb56a471aab54d020c18fb90c20b474b1775fcc
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-01 22:34:36 +0530

    Refactor CausalSelfAttention initialization and AdamW class

--- model.py
+++ model.py
@@ -32,58 +32,58 @@ class LayerNorm(nn.Module):
         """
 	  
 class CausalSelfAttention(nn.Module):
-    def __init__(self, config:"GPTConfig"):
-        assert config.n_embd % config.n_head == 0
-        super().__init__()
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropout
-        self.bias = config.bias
-        self.c_attn = nn.Linear(self.n_embd, 3 * self.n_embd, bias=self.bias)
-        self.c_proj = nn.Linear(self.n_embd, self.n_embd, bias=self.bias)
-        '''
-        nn.linear
-        1. input @ w.T + bias
-        2. weight, bias will be created by itself using given shape
-        3. nn.Linear(in_features, out_features, bias=True)
-        4. weight = torch.randn(out_features, in_features)
-        5. bias = torch.randn(out_features)
-        '''
-        # remember dropout is always between 0 - 1
-        # nn.Dropout(value, inplace=True/False) -> inplace is an optional and this inplace is to create a new tensor or inplace
-        self.attn_dropout = nn.Dropout(self.dropout)
-        self.resid_dropout = nn.Dropout(self.dropout)
-        #------------------------------------------------------------------------
-        '''
-        check our PyTorch >= 2.0, then scaled_dot_product_attention will be there
-        scaled_dot_product_attention = softmax((Q.K_T)/sqrt(channel)).V
-        '''
-        self.flash = hasattr(F, 'scaled_dot_product_attention')
-        if not self.flash: # older version
-	        print("WARNING: using slow attention. Flash attention requires PyTorch >= 2.0")
-	        '''
-	        1. this created tensor will be in the model, but don't train it
-	        2. this is a templace 
-	        [1,0,0]
-	        [1,1,0]
-	        [1,1,1]
-	        3. [1] * block_size -> reshape it to -> (1, 1, block_size, block_size)
-	        4. and to the model buffer
-	        '''
-	        self.register_buffer(
-	                "bias",
-	                torch.tril(
-	                torch.ones(config.block_size, config.block_size)
-	                ).view(1, 1, config.block_size, config.block_size)
-	        )  
-			
-    def forward(self, x:Tensor)->Tensor:
+	def __init__(self, config:"GPTConfig"):
+		assert config.n_embd % config.n_head == 0
+		super().__init__()
+		self.n_head = config.n_head
+		self.n_embd = config.n_embd
+		self.dropout = config.dropout
+		self.bias = config.bias
+		self.c_attn = nn.Linear(self.n_embd, 3 * self.n_embd, bias=self.bias)
+		self.c_proj = nn.Linear(self.n_embd, self.n_embd, bias=self.bias)
+		'''
+		nn.linear
+		1. input @ w.T + bias
+		2. weight, bias will be created by itself using given shape
+		3. nn.Linear(in_features, out_features, bias=True)
+		4. weight = torch.randn(out_features, in_features)
+		5. bias = torch.randn(out_features)
+		'''
+		# remember dropout is always between 0 - 1
+		# nn.Dropout(value, inplace=True/False) -> inplace is an optional and this inplace is to create a new tensor or inplace
+		self.attn_dropout = nn.Dropout(self.dropout)
+		self.resid_dropout = nn.Dropout(self.dropout)
+		#------------------------------------------------------------------------
+		'''
+		check our PyTorch >= 2.0, then scaled_dot_product_attention will be there
+		scaled_dot_product_attention = softmax((Q.K_T)/sqrt(channel)).V
+		'''
+		self.flash = hasattr(F, 'scaled_dot_product_attention')
+		if not self.flash: # older version
+			print("WARNING: using slow attention. Flash attention requires PyTorch >= 2.0")
+			'''
+			1. this created tensor will be in the model, but don't train it
+			2. this is a templace 
+			[1,0,0]
+			[1,1,0]
+			[1,1,1]
+			3. [1] * block_size -> reshape it to -> (1, 1, block_size, block_size)
+			4. and to the model buffer
+			'''
+			self.register_buffer(
+					"bias",
+					torch.tril(
+					torch.ones(config.block_size, config.block_size)
+					).view(1, 1, config.block_size, config.block_size)
+			)  
+		 
+	def forward(self, x:Tensor)->Tensor:
 		B, T, C = x.size()
 		q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
 		q = q.view(B, T, self.n_head, C//self.n_head).transpose(1, 2)
 		k = k.view(B, T, self.n_head, C//self.n_head).transpose(1, 2)
 		v = v.view(B, T, self.n_head, C//self.n_head).transpose(1, 2)
-	
+
 		if self.flash:
 			y = F.scaled_dot_product_attention(
 				q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True
@@ -288,39 +288,40 @@ class GPT(nn.Module):
 		optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
 		return optimizer
 
-'''
-torch.optim.AdamW, Simplified Explanation
+	'''
+	torch.optim.AdamW, Simplified Explanation
 
-class AdamW:
-    def __init__(self, optim_groups, lr, betas):
-        self.groups = optim_groups
-        self.lr = lr
-        self.beta1, self.beta2 = betas
-        self.m, self.v = {}, {}
-        self.step = 0
-	#----------------------------------------------
-    def step(self):
-        self.step += 1
-        for group in self.groups:
-            for w in group["params"]:
-                g = w.grad
-                if w not in self.m:
-                    self.m[w] = self.v[w] = 0
+	class AdamW:
+		def __init__(self, optim_groups, lr, betas):
+			self.groups = optim_groups
+			self.lr = lr
+			self.beta1, self.beta2 = betas
+			self.m, self.v = {}, {}
+			self.step = 0
+		#----------------------------------------------
+		def step(self):
+			self.step += 1
+			for group in self.groups:
+				for w in group["params"]:
+					g = w.grad
+					if w not in self.m:
+						self.m[w] = self.v[w] = 0
+						
+					# Gradient history
+					self.m[w] = self.beta1 * self.m[w] + (1 - self.beta1) * g
+					# Squared-gradient history
+					self.v[w] = self.beta2 * self.v[w] + (1 - self.beta2) * g**2
 					
-				# Gradient history
-                self.m[w] = self.beta1 * self.m[w] + (1 - self.beta1) * g
-				# Squared-gradient history
-                self.v[w] = self.beta2 * self.v[w] + (1 - self.beta2) * g**2
-				
-				# Bias correction
-                m_hat = self.m[w] / (1 - self.beta1**self.step)
-                v_hat = self.v[w] / (1 - self.beta2**self.step)
+					# Bias correction
+					m_hat = self.m[w] / (1 - self.beta1**self.step)
+					v_hat = self.v[w] / (1 - self.beta2**self.step)
 
-                # Adam
-                w -= m_hat * self.lr / (v_hat.sqrt() + 1e-8)
-                # AdamW
-                w -= w * self.lr * group["weight_decay"]
-'''
+					# Adam
+					w -= m_hat * self.lr / (v_hat.sqrt() + 1e-8)
+					# AdamW
+					w -= w * self.lr * group["weight_decay"]
+	'''
+	
 	def estimate_mfu(self, fwdbwd_per_iter, dt):# need to learn
 		""" estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
 		N = self.get_num_params()
@@ -368,14 +369,14 @@ class AdamW:
 			idx = torch.cat((idx, idx_next), dim=1) 
 			'''
 			i guess dim = 2
-			  idx      |   idx_next
+				idx      |   idx_next
 			--------------------------
 			[[1, 2, 3], | [[9, 10],
 			[5, 6, 7]]  |  [11, 12]]
 			---------------------------
 			result:
 			[[1, 2, 3, 9, 10],
-			 [5, 6, 7, 11, 12]]
+				[5, 6, 7, 11, 12]]
 			'''
 		return idx
 			

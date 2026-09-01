89a27da04cc61a58d9e8831ea346c4e705d1f464
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-01 20:20:30 +0530

    Enhance estimate_mfu with FLOPs calculation details
    Added detailed comments for estimating model FLOPs utilization in the estimate_mfu function.

--- model.py
+++ model.py
@@ -320,12 +320,24 @@ class AdamW:
 '''
 	def estimate_mfu(self, fwdbwd_per_iter, dt):# need to learn
 		""" estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
-		# first estimate the number of flops we do per iteration.
-		# see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
 		N = self.get_num_params()
 		cfg = self.config
 		L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd//cfg.n_head, cfg.block_size
+		'''
+		6 * N:
+		2 -> foward pass    
+		4 -> baclward pass (2 times of forward_Pass) 
+		------------------------------------------
+		12*L*H*Q*T:
+		QKᵀ  → roughly 2 × T² × H × Q FLOPs
+		AV   → roughly 2 × T² × H × Q FLOPs
+		4*T²*H*Q:
+		forward_Pass = 4*T²*H*Q
+		backwark_Pass = 8*T²*H*Q (2 times of forward_Pass)
+		------------------------------------------
+		'''
 		flops_per_token = 6*N + 12*L*H*Q*T
+		# H * Q => Embedding dimensions
 		flops_per_fwdbwd = flops_per_token * T
 		flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
 		# express our flops throughput as ratio of A100 bfloat16 peak flops

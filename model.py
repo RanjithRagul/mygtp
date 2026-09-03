Fix tensor transpose dimensions in attention calculation
Updated tensor transpose dimensions for attention mechanism and adjusted subsequent operations.

model.py
@@ -92,13 +92,14 @@ class CausalSelfAttention(nn.Module):
 				is_causal=True
 			)
 		else:
-			att = q @ k.transpose(-2, -1) / math.sqrt(k.size(-1))
+			att = q @ k.transpose(1, 2) / math.sqrt(k.size(-1)) # here (1, 2) != (-1, -2) because its a 4D
 			att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
 			att = F.softmax(att, dim=-1)
 			att = self.attn_dropout(att)
 			y = att @ v
-		y = y.transpose(1, 2).contiguous().view(B, T, C)
-		y = self.resid_dropout(self.c_proj(y))
+		y = y.transpose(-2, -1).contiguous().view(B, T, C)
+		y = self.c_proj(y)
+		y = self.resid_dropout(y)
 		return y
 		
 class MLP(nn.Module):
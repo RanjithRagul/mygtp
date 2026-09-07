75d49e140340c56ab5eeffedaa75b17ecefcf8f6
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-07 19:34:04 +0530

    Refactor training script with updated imports and logic

--- train.py
+++ train.py
@@ -153,4 +153,4 @@ elif init_from.startwith('gpt2'):
 if block_size < model.config.block_size:
   mode.crop_block_size(block_size)
   model_args['block_size'] = block_size
-model.size(device)
+model.to(device)

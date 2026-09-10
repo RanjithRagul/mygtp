751fa7a2703e1bda8bc2f6ab174dda7c1722cc12
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-10 22:29:51 +0530

    Add type annotations for weight and bias in LayerNorm

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ from dataclasses import dataclass
 class LayerNorm(nn.Module):
     def __init__(self, ndim:int, bias:bool):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+        self.weight : Tensor        = nn.Parameter(torch.ones(ndim))
+        self.bias   : Tensor | None = nn.Parameter(torch.zeros(ndim)) if bias else None
     
     def forward(self, input: Tensor) -> Tensor:
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)

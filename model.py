5ae4923c3074c6dff9a6ee0533db9b9153a3ef51
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-10 16:00:49 +0530

    Simplify parameter dictionary creation in optimizers

--- model.py
+++ model.py
@@ -279,8 +279,7 @@ class GPT(nn.Module):
 		return model
 		
 	def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
-		param_dict = {pn:p for pn, p in self.named_parameters()}
-		param_dict = {pn:p for pn, p in param_dict.items() if p.requires_grad}
+		param_dict = {pn:p for pn, p in self.named_parameters() if p.requires_grad}
 		decay_params = [p for p in param_dict.values() if p.dim() > 1]
 		nodecay_params = [p for p in param_dict.values() if p.dim() < 2]
 		optim_groups = [

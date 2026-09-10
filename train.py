5432cbf497cdda35dab93584a9c44cd940c4e36c
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-10 22:56:47 +0530

    Refactor train.py for improved readability and structure

--- train.py
+++ train.py
@@ -154,7 +154,7 @@ elif init_from == 'resume':
       
   model.load_state_dict(state_dict)
   iter_num = checkpoint['iter_num']
-  best_val+loss = checkpoint['best_val_loss']
+  best_val_loss = checkpoint['best_val_loss']
 elif init_from.startwith('gpt2'):
   print('Initializing from OpenAI GPT-2 weights: {init_from}')
   override_args = dict(dropout=dropout)

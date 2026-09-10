adf05be2b1e5687f5358625279a058171bd1f43b
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-10 23:01:59 +0530

    Refactor training script and fix syntax errors

--- train.py
+++ train.py
@@ -74,7 +74,7 @@ token_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * blo
 print(f'toeks per iteration will be: {token_per_iter:,}')
 
 if master_process:
-  os.makedir(out_dir, exist_ok=True)
+  os.makedirs(out_dir, exist_ok=True)
 
 torch.backend.cuda.matmul.allow_tf32 = True
 torch.backend.cudnn.allow_tf32 = True

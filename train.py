e3d641df039578642966f3f8228c2b928c8011c0
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-11 16:07:54 +0530

    Update train.py

--- train.py
+++ train.py
@@ -51,7 +51,8 @@ gradient_Accumulation = 5 * 8
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
 #--------------------------------------------------------------------------
 backend = 'nccl' # gloo, etc
-ddp = int(os.environ.get('RANK', -1)) != -1
+ddp     = int(os.environ.get('RANK', -1)) != -1
+
 if ddp: # GPU
   init_process_groups(backend=backend)
   ddp_rank       = int(os.environ['RANK'])
@@ -61,13 +62,13 @@ if ddp: # GPU
   device = f'cuda:{dpp_local_rank}'
   torch.cuda.set_device('device')
   master_process = ddp_rank ==  0
-  seed_offset = ddp_rank
+  seed_offset    = ddp_rank
 
   assert gradient_Accumulation % ddp_world_size == 0
   gradient_accumulation_stes //= ddp_world_size
 else: # CPU
   master_process = True
-  seed_offset = 0
+  seed_offset    = 0
   ddp_world_size = 1
 
 token_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * block_size
@@ -77,24 +78,26 @@ if master_process:
   os.makedirs(out_dir, exist_ok=True)
 
 torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.allow_tf32       = True
 device_type = 'cuda' if 'cuda' in device else 'cpu'
 ptdtype     = {
                 'float32'  : torch.float32,
                 'float16'  : torch.float16,
                 'bfloat16' : torch.bfloat16,
               }[dtype]
+
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 data_dir = os.path.join('data', dataset)
 def get_batch(split):
   if split == 'train':
-    data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+    data   = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
   else:
     data = np.memmap(os.path.join(data_dir, 'val.bin'  ), dtype=np.uint16, mode='r')
+    
   ix = torch.ranint(len(data) - block_size, (batch_size,))
-  x = torch.stack([torch.from_numpy((data[i  :i+block_size ]).astype(np.int64))  for i in ix])
-  y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in id])
+  x  = torch.stack([torch.from_numpy((data[i  :i+block_size ]).astype(np.int64))  for i in ix])
+  y  = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in id])
 
   if device_type == 'cuda':
     x = x.pin_memory().to(device, non_blocking=True)
@@ -111,6 +114,7 @@ bext_val_loss = 1e9 # 1000,000,000 = 1Billion
 # attempt to derive vocab_size from the dataset
 meta_path = os.path.join(data_dir, 'meta.pkl')
 meta_vocab_size = None
+
 if os.path.exists(meta_path):
   with open(meta_path, 'rb') as f:
     meta = pickle.load(f)
@@ -120,25 +124,28 @@ if os.path.exists(meta_path):
 model_args   = dict(
                     block_size = block_size,
                     vocab_size = None,
-                    n_embd  = n_embd,
-                    n_layer = n_layer,
-                    n_head  = n_head,
-                    dropout = dropout,
-                    bias    = bias
+                    n_embd     = n_embd,
+                    n_layer    = n_layer,
+                    n_head     = n_head,
+                    dropout    = dropout,
+                    bias       = bias
                   )
 
 if init_from == 'scratch':
   print(f'Initializing a new model from scratch')
   if meta_vocab_size is None:
     print('Defaulting to vocab_size of GPT-2 to 50304 (50257 rounded up for efficiency)')
+    
   model_args['vocab_size'] = meta_vocab_size if meta_vocab_size is not None else 50304
   gptconfig = GPTConfig(**model_args)
   model = GPT(gptconfig)
+  
 elif init_from == 'resume':
   print(f'Resuming training from {out_dir}')
   ckpt_path  = os.path.join(out_dir, 'ckpt.pt')
   checkpoint = torch.load(ckpt_path, map_location = device)
   checkpoint_model_args = checkpoint['model_args']
+  
   for k in ['block_size', 'vocab_size', 'n_embd', 'n_layer', 'n_head', 'bias']:
     model_args[k] = checkpoint_model_args[k]
 
@@ -148,6 +155,7 @@ elif init_from == 'resume':
   state_dict = checkpoint['model']
   unwanter_prefix = '_orgin_mod.'
   N_unwanter_prefix = len(unwanted_prefix)
+  
   for k in state_dict.keys():
     if k.startswith(unwanted_prefix):
       state_dict[k[N_unwanted_prefix:]] = state_dict.pop(k)
@@ -155,6 +163,7 @@ elif init_from == 'resume':
   model.load_state_dict(state_dict)
   iter_num = checkpoint['iter_num']
   best_val_loss = checkpoint['best_val_loss']
+  
 elif init_from.startwith('gpt2'):
   print('Initializing from OpenAI GPT-2 weights: {init_from}')
   override_args = dict(dropout=dropout)
@@ -184,8 +193,8 @@ but float16, 0.00000001 set to be 0. we will lost the accuracy
 9. update scale
 '''
 scaler = torch.cuda.amp.GradScaler(enabled=dtype=='float16')
-
 optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
+
 if init_from == 'resume':
   optimizer.load_state_dict(checkpoint['optimizer'])
 checkpoint = None
@@ -210,7 +219,7 @@ def estimate_loss():
       with cts:
         logits, loss = model(X, Y)
       losses[k] = loss.item()
-    out[split] = losses.mean()
+    out[split]  = losses.mean()
   model.train()
   return out
 
@@ -274,6 +283,7 @@ while True:
     with ctx:
       logits, loss = model(X, Y)
       loss = loss / gradient_accumulation_steps
+      
     X, Y = get_batch('train')
     scaler.scale(loss).backward()
 
